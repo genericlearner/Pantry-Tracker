9a7fa1300e7bea67264afc335622558a0dd543f7
genericlearner <umar.akahyd@gmail.com>
2024-08-01 20:28:28 -0400

    Added buttons with the working functionality of adding and removing items

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 'use client'
-
 import {useState,useEffect} from 'react'
 //import {firestore} from 'firebase'
-import {Box, Stack, Typography} from '@mui/material'
-import {query, collection, getDocs, docRef, setDoc} from 'firebase/firestore'
+import {Box, Stack, Modal, Typography, TextField, Button} from '@mui/material'
+import {query, collection, getDoc, setDoc, doc, deleteDoc, getDocs} from 'firebase/firestore'
 import { firestore } from './firebase'
 
 
@@ -59,21 +58,95 @@ export default function Home() {
   useEffect(()=>{
     updateInventory()
   },[])
-  return (
-    <Box>
-      <Typography variant="h1">Inventory Management</Typography>
-      {
-        inventory.forEach((item)=>{
 
-          return(<Box>
-          
-          {item.name}
-          {item.count}
-          </Box>)
-        })
-      }
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  return (
+    <Box width = "100vw"
+    height="100vh"
+    display = "flex"
+    flexDirection="column"
+    justifyContent="center"
+    alignItems="center"
+    gap={2}
+    >
+      <Modal open = {open} onClose = {handleClose}>
+        <Box position = "absolute" 
+        top= "50%" 
+        left ="50%" 
+        width={400} bgcolor="white" 
+        border="2px solid #000"
+        boxShadow={24}
+        p={4}
+        display="flex"
+        flexDirection="column"
+        gap={3}
+        sx={{
+          transform: 'translate(-50%,-50%)',
+        }}>
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField 
+            variant='outlined'
+            fullWidth
+            value = {itemName}
+            onChange={(e)=>{
+              setItemName(e.target.value)
+            }}
+            /><Button variant="outlined" onClick = {()=>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}>
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={()=>{
+        handleOpen()
+      }}>
+        Add new Item
+      </Button>
+      <Box border='1px solid #333'>
+        <Box 
+        width="800px" 
+        height="100px" 
+        bgcolor= '#ADD8E6' 
+        display = "flex" 
+        alignItems="center" 
+        justifyContent="center">
+          <Typography variant = 'h2' color = '#333'>
+            Inventory Items
+          </Typography>
+        </Box>
       
-    
+      <Stack width="800px" height="300px" spacing={2} overflow="auto">
+        {
+          inventory.map(({name, quantity})=>(
+            <Box key={name} 
+            width="100%" 
+            minHeight="150px" 
+            display="flex" 
+            alignItems="center" 
+            justifyContent="space-between" 
+            bgColor="#f0f0f0" 
+            padding={5}>
+              <Typography variant = 'h3' color = '#333' textAlign="center">
+                {name.charAt(0).toUpperCase()+name.slice(1)}
+              </Typography>
+              <Typography variant = 'h3' color = '#333' textAlign="center">
+                {quantity}
+              </Typography>
+              <Button variant = "contained" onClick={()=>{
+                removeItem(name)
+              }}>
+                Remove 
+              </Button>
+            </Box>
+          ))}
+      </Stack>
+      </Box>
     </Box>
    
   );
